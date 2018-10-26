7a652978133adeeae174a2672f2e475b865af244
SANJAY K SAI <shachsan@hotmail.com>
2018-10-26 17:07:56 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+	if speed > 60 || speed < 40
+		return true 
+	else 
+		return false 
+	end 
+		
 end
 
 
 
 def not_safe?(speed)
-	
+	return true if speed > 60 || speed < 40
+	return false if speed >=40 && speed <=60
 end
 	
 
